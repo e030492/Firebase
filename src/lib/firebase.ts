f0e6aa890a5643224b1b9eaf2084ee59d4c72ff7
Erick Lopez <e030492@hotmail.com>
2025-07-23 21:05:07 +0000

    ya publique el proyecto y continua con el error, solo muestra conectando

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  "projectId": "guardian-shield-k9g9l",
+  "appId": "1:224891357938:web:399bcf1f064bdf7bdf9e6b",
+  "storageBucket": "guardian-shield-k9g9l.firebasestorage.app",
+  "apiKey": "AIzaSyC6MOe2IHjiMxGLl2aEAqg2KKvsAcnd-ZM",
+  "authDomain": "guardian-shield-k9g9l.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "224891357938"
 };
 
 // Initialize Firebase
